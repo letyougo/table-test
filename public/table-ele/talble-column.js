138f212c5b898af6a8b8b2ac1bda723e28822425
surui1 <surui1@xiaomi.com>
2018-07-30 09:11:27 +0800

    first commit

---
+++ public/table-ele/talble-column.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+export default class TableColumn extends Component{
+  render(){
+    let {data,index} = this.props
+    return (
+      <tr>
+        <td>111</td>
+      </tr>
+    )
+  }
+}
